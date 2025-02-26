71c681a0c62b78e8bb24034da9b48227348f7863
AbhinRustagi <rustagi.abhin@gmail.com>
2025-02-26 17:30:09 +0530

    feat: Add firebase

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,18 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  env: {
+    NEXT_APP_FIREBASE_API_KEY: process.env.NEXT_APP_FIREBASE_API_KEY,
+    NEXT_APP_FIREBASE_AUTH_DOMAIN: process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
+    NEXT_APP_FIREBASE_PROJECT_ID: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
+    NEXT_APP_FIREBASE_STORAGE_BUCKET:
+      process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
+    NEXT_APP_FIREBASE_MESSAGING_SENDER_ID:
+      process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_APP_FIREBASE_APP_ID: process.env.NEXT_APP_FIREBASE_APP_ID,
+    NEXT_APP_FIREBASE_MEASUREMENT_ID:
+      process.env.NEXT_APP_FIREBASE_MEASUREMENT_ID,
+  },
 };
 
 export default nextConfig;
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,1 +1,20 @@
-export {};
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { Place } from "./types";
+
+const app = initializeApp({
+  apiKey: process.env.NEXT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_APP_FIREBASE_APP_ID,
+});
+
+export const db = getFirestore(app);
+
+export async function getRestaurants(): Promise<Place[]> {
+  const ref = collection(db, "eats_restaurants");
+  const docs = await getDocs(ref);
+  return docs.docs.map((doc) => doc.data()) as Place[];
+}
