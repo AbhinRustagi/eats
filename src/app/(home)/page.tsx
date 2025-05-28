479bebb34b036292446d56c50976844ccbb3bbf2
AbhinRustagi <rustagi.abhin@gmail.com>
2025-05-28 20:16:25 +1000

    feat: add dynamic rendering for SSR

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import { getRestaurants } from "@/lib/firebase";
 import { buildConfigMap } from "@/lib/utils";
 import PageContent from "./_client";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const restaurants = await getRestaurants();
   const configMap = buildConfigMap(restaurants);
